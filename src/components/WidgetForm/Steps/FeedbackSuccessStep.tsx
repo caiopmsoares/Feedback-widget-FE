fb0dc046049426e3611ee003a623afe25f9674a6
Caio Macedo <caiopierre7@hotmail.com>
2022-06-09 12:41:51 -0300

    Finalizando a função de captura de imagem e envio do formulário

--- src/components/WidgetForm/Steps/FeedbackSuccessStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackSuccessStep.tsx
@@ -1,3 +1,36 @@
+import { CloseButton } from '../../CloseButton';
+
 export function FeedbackSuccessStep() {
-	return null;
+	return (
+		<>
+			<header>
+				<CloseButton />
+			</header>
+
+			<div className="flex flex-col items-center py-10 w-[304px]">
+				<svg
+					width="48px"
+					height="48px"
+					viewBox="0 0 48 48"
+					version="1"
+					xmlns="http://www.w3.org/2000/svg"
+					enable-background="new 0 0 48 48"
+				>
+					<circle fill="#4CAF50" cx="24" cy="24" r="21" />
+					<polygon
+						fill="#FFF"
+						points="34.6,14.6 21,28.2 15.4,22.6 12.6,25.4 21,33.8 37.4,17.4"
+					/>
+				</svg>
+
+				<span className="text-xl mt-2">Agradecemos o feedback!</span>
+				<button
+					type="button"
+					className="py-2 px-6 mt-6 bg-zinc-800 rounded-md border-transparent text-sm leading-6 hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500"
+				>
+					Quero enviar outro
+				</button>
+			</div>
+		</>
+	);
 }
